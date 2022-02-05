3e2b68bd2f2df817353e6f7d4107786ff1b6da2a
Daniel Bedoya <danie@MacBook-Pro.local>
2022-02-05 14:07:51 -0800

    First Exercise

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Forma corta
+// const Title = ({course}) => <h1>{course}</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Header = ({name}) => (
+  <h1>{name}</h1>
+)
+
+const Part = (props) => (
+    <p>{props.name} {props.exercises}</p>
+  )
+
+
+const Total = ({parts}) => {
+  let numberOfExercises = 0
+  parts.forEach(part => {
+    console.log(part.exercises)
+    numberOfExercises += part.exercises
+    console.log(numberOfExercises)
+  })
+  return(
+    <p>Number of exercises {numberOfExercises}</p>
+  )
+}
+
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts:[{
+    name:'Fundamentals of React',
+    exercises:10},
+    {name:'Using props to pass data',
+    exercises:7},
+    {name: 'State of a component',
+    exercises:14}]}
+  
+
+  const Content = ({parts}) => {
+    // const numberOfExercises = props.parts.map(exercise => exercise['exercises'] += exercise['exercises'])
+    return(
+    parts.map(part => <Part name = {part.name} exercises = {part.exercises}/>)
+    )
+  }
+
+  return (
+    <div>
+      <Header name = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
